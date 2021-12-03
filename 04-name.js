3ed8f9ef20be2d17baa8a37122420bcc1610406b
Anubhav <anubhavanand3007@gmail.com>
2021-12-03 10:48:24 +0530

    second minimal

---
+++ 04-name.js
@@ -0,0 +1,9 @@
+//local:
+const secret  = "SECRET KEY"
+
+//public:
+const name = "Anubhav"
+const second_name = "Anand"
+const class_name = "work or break"
+
+module.exports = {name, second_name, class_name}
